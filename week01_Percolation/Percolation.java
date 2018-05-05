e21cf729668dd5c12ca1d9288a1216da55bed3e2
Yuto Tagashira <gh0st1nth3sh311.dev@gmail.com>
2018-05-06 01:01:12 +0900

    refactor: !!!Bug Exist!!!

--- week01_Percolation/Percolation.java
+++ week01_Percolation/Percolation.java
@@ -8,90 +8,131 @@ public class Percolation {
   WeightedQuickUnionUF qu_sites;
 
   public Percolation(int n) {// create n-by-n grid, with all sites blocked
+    if (n <= 0)
+      throw new IllegalArgumentException("n is less than or equal to 0");//error hanndling
 
-    this.sites = new boolean[n * n];
+    this.n        = n;
+    this.qu_sites = new WeightedQuickUnionUF(n * n + 2);//add two dummies(virtual top and bottom)
+    this.sites    = new boolean[n * n];
 
     for (int i = 0; i < n * n; i++) {
-      sites[i] = False;//False:blocked,True:open
+      this.sites[i] = false;//False:blocked,True:open
     }
-
-    qu_sites = new WeightedQuickUnionUF(n * n + 2);
 
     //connect virtual top site
-    for (int i = 1; i <= n; i++) {
-      qu_sites.union(0, i);
+    for (int i = 0; i < n; i++) {
+      this.qu_sites.union(n * n, i);
     }
     //connect virtual bottom site
-    for (int i = 1; i <= n; i++) {
-      qu_sites.union(n * (n - 1) + 2 - 1 + i, n * n + 2 - 1);
+    for (int i = (0 + n * n - n-1); i < n; i++) {
+      this.qu_sites.union(i, n * n + 1);
     }
   }
 
-  public void open(int row, int col) {// open site (row, col) if it is not open already
-    if (!isOpen(row, col)) {
-      sites[row + col * n] = 1;
+  private void validateIndex(int x, int y) {
+    if (!isValid(x, y)) {
+      throw new IndexOutOfBoundsException(String.format("N:%d x:%d y:%d", this.n, x, y));
+    }
+  }
 
-      if (isOpen(row - 1, col)) {
-        qu_sites.union(1 + row + col * n, 1 + row - 1 + col * n);
+  private boolean isValid(int x, int y) {
+    return x >= 1 && x <= this.n && y >= 1 && y <= this.n && x+y*this.n-1 < n*n;//n*n and n*n+2-1 are dummies
+  }
+
+  public void open(int row, int col) {// open site (row, col) if it is not open already
+    if (isValid(row, col) && !isOpen(row, col)) {
+      System.out.printf("Open site[%d, %d]\n", row, col);
+      this.sites[row + col * n - 1] = true;
+
+      if (col == 0) {
+        System.out.printf("Open up site\n");
+        System.out.printf("Connect Dummy Top\n");
+        this.qu_sites.union(row + col * n - 1, n * n);
+      } else if (col == n) {
+        System.out.printf("Open down site\n");
+        System.out.printf("Connect Dummy Bottom\n");
+        this.qu_sites.union(row + col * n - 1, n * n + 1);
+      } else {
+        if (isValid(row - 1, col) && isOpen(row - 1, col)) {
+          System.out.printf("Open down site\n");
+          this.qu_sites.union(row + col * n - 1, row - 1 + col * n - 1);
+        }
+        if (isValid(row + 1, col) && isOpen(row + 1, col)) {
+          System.out.printf("Open up site\n");
+          this.qu_sites.union(row + col * n - 1, row + 1 + col * n - 1);
+        }
       }
-      if (isOpen(row + 1, col)) {
-        qu_sites.union(1 + row + col * n, 1 + row + 1 + col * n);
+      if (isValid(row, col - 1) && isOpen(row, col - 1)) {
+        System.out.printf("Open left site\n");
+        this.qu_sites.union(row + col * n - 1, row + (col - 1) * n - 1);
       }
-      if (isOpen(row, col - 1)) {
-        qu_sites.union(1 + row + col * n, 1 + row + (col - 1) * n);
-      }
-      if (isOpen(row, col + 1)) {
-        qu_sites.union(1 + row + col * n, 1 + row + (col + 1) * n);
+      if (isValid(row, col + 1) && isOpen(row, col + 1)) {
+        System.out.printf("Open right site\n");
+        this.qu_sites.union(row + col * n - 1, row + (col + 1) * n - 1);
       }
     }
   }
 
   public boolean isOpen(int row, int col) {// is site (row, col) open?
-    if (sites[row + col * n] == 1) {
-      return true;
-    } else {
+    if (isValid(row, col)) {
+      try {
+        return this.sites[row + col * n - 1];
+      } catch (IndexOutOfBoundsException e) {
+        System.out.println("Error " + e.getMessage());
+        //      e.printStackTrace();
+      }
+    }
+    return false;
+  }
+
+  public boolean isFull(int row, int col) {// is site (row, col) full?
+    try {
+      return this.qu_sites.connected(0, 1 + row + col * n);
+    } catch (IndexOutOfBoundsException e) {
+      System.out.println("Error " + e.getMessage());
+      //      e.printStackTrace();
       return false;
     }
   }
 
-  public boolean isFull(int row, int col) {// is site (row, col) full?
-    return qu_sites.connected(0, 1 + row + col * n);
-  }
-
   public int numberOfOpenSites() {// number of open sites
     int numberOfOpen = 0;
-    for (int i = 0; i < n * n; i++) {
-      if (isOpen(i % n, i / n)) {//0:blocked,1:open
-        numberOfOpen++;
+    for (int i = 0; i < n; i++) {
+      for(int j=0;j<n;j++){
+        if(isOpen(i,j)){
+          numberOfOpen++;
+          System.out.printf("Count ....");
+        }
       }
+      
     }
     return numberOfOpen;
   }
 
   public boolean percolates() {// does the system percolate?
-    return qu_sites.connected(0, n * n + 2);
+    return this.qu_sites.connected(n*n, n * n + 1);
   }
 
-  public static void main(String[] args) {// test client (optional)
-    test2();
-  }
+  // public static void main(String[] args) {// test client (optional)
+  //   test2();
+  // }
 
-  private static void test2() {
-    final Percolation p = new Percolation(3);
-    System.out.println("p.isOpen(1, 2) = " + p.isOpen(1, 2));
-    p.open(1, 2);
-    System.out.println("p.isOpen(1, 2) = " + p.isOpen(1, 2));
+  // private static void test2() {
+  //   final Percolation p = new Percolation(3);
+  //   System.out.println("p.isOpen(1, 2) = " + p.isOpen(1, 2));
+  //   p.open(1, 2);
+  //   System.out.println("p.isOpen(1, 2) = " + p.isOpen(1, 2));
 
-    System.out.println("p.isOpen(2,2) = " + p.isOpen(2, 2));
-    p.open(2, 2);
-    System.out.println("p.isOpen(2,2) = " + p.isOpen(2, 2));
-    System.out.println("p.isFull(2, 2) = " + p.isFull(2, 2));
+  //   System.out.println("p.isOpen(2,2) = " + p.isOpen(2, 2));
+  //   p.open(2, 2);
+  //   System.out.println("p.isOpen(2,2) = " + p.isOpen(2, 2));
+  //   System.out.println("p.isFull(2, 2) = " + p.isFull(2, 2));
 
-    System.out.println("p.isOpen(3, 1) = " + p.isOpen(3, 2));
-    p.open(3, 2);
-    System.out.println("p.isOpen(3, 1) = " + p.isOpen(3, 2));
-    p.isFull(3, 2);
+  //   System.out.println("p.isOpen(3, 1) = " + p.isOpen(3, 2));
+  //   p.open(3, 2);
+  //   System.out.println("p.isOpen(3, 1) = " + p.isOpen(3, 2));
+  //   p.isFull(3, 2);
 
-    System.out.println("p.percolates() = " + p.percolates());
-  }
+  //   System.out.println("p.percolates() = " + p.percolates());
+  // }
 }
--- week01_Percolation/PercolationStats.java
+++ week01_Percolation/PercolationStats.java
@@ -2,11 +2,11 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import Percolation;
+//import Percolation;
 
 public class PercolationStats {
-  int n;
-  int trials;
+  static int n;
+  static int trials;
 
   double[] result;
   double mean;
@@ -15,49 +15,61 @@ public class PercolationStats {
   double high;
 
   public PercolationStats(int n, int trials) {// perform trials independent experiments on an n-by-n grid
+    if (n <= 0 || trials <= 0)
+      throw new IllegalArgumentException("Arguments out of bound");
+
     Percolation percolate = new Percolation(n);
+    System.out.printf("making new instance\n");
 
-    double[] result = new double[trials];
-
-    StdRandom random = new StdRandom(n);
+    this.result = new double[trials];
 
     for (int i = 0; i < trials; i++) {
       while (!percolate.percolates()) {
-        percolate.open(random, random);
-        result[i] = percolate.numberOfOpenSites() / (n * n);
+        //        System.out.printf("Generate Random Numbers\n");
+        int row    = StdRandom.uniform(1, n + 1);
+        int column = StdRandom.uniform(1, n + 1);
+//        if (percolate.isOpen(row, column)) {
+//          System.out.printf("isOpen \n");
+//          continue;
+//        }
+        percolate.open(row, column);
       }
+      System.out.printf("Percolated\n");
+      this.result[i] = percolate.numberOfOpenSites() / (n * n);
+      System.out.printf("result[%d] = %f\n", i, result[i]);
     }
   }
 
   public double mean() {// sample mean of percolation threshold
-    mean = StdStats.sum(result) / trials;
+    mean = StdStats.mean(result);
     return mean;
   }
 
   public double stddev() {// sample standard deviation of percolation threshold
     stddev = StdStats.stddev(result);
+    return stddev;
   }
 
   public double confidenceLo() {// low  endpoint of 95% confidence interval
-    low = mean - 1.96 * stddev / sqrt(trials);
+    low = mean - 1.96 * stddev / Math.sqrt(trials);
     return low;
   }
 
   public double confidenceHi() {// high endpoint of 95% confidence interval
-    high = mean = -1.96 * stddev / sqrt(trials);
+    high = mean = -1.96 * stddev / Math.sqrt(trials);
     return high;
   }
 
   public static void main(String[] args) {// test client (described below)
-    n      = args[0];
-    trials = args[1];
+    n      = Integer.parseInt(args[0]);
+    trials = Integer.parseInt(args[1]);
 
-    PercolationStats(n, trials);
+    PercolationStats pls = new PercolationStats(n, trials);
     // mean                    = 0.5929934999999997
     // stddev                  = 0.00876990421552567
     // 95% confidence interval = [0.5912745987737567, 0.5947124012262428]
-    StdOut.printf("mean %10.3f\n", mean);
-    StdOut.printf("stddev %10.3f\n", stddev);
-    StdOut.printf("95% confidence interval = [%10.3f, %10.3f]\n", low, high);
+    System.out.printf("mean %10.3f\n", pls.mean());
+    System.out.printf("stddev %10.3f\n", pls.stddev());
+    System.out.printf("95% confidence interval = [%10.3f, %10.3f]\n", pls.confidenceLo(), pls.confidenceHi());
   }
 }
