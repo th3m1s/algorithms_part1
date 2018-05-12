09dc29140ef81bdad29e6a623df311a81ccede45
Yuto Tagashira <gh0st1nth3sh311.dev@gmail.com>
2018-05-12 14:35:35 +0900

    update: passed at 80/100 points

--- week01_Percolation/Percolation.java
+++ week01_Percolation/Percolation.java
@@ -1,7 +1,11 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-  private int n;
+  private final int n;
+
+  // positional pointers to virtual top and bottom of UF data structure
+  private final int vTop, vBottom;
+
   private WeightedQuickUnionUF quSites;
   private boolean[] sites;
 
@@ -10,20 +14,21 @@ public class Percolation {
       throw new IllegalArgumentException("n is less than or equal to 0"); // error hanndling
 
     this.n = n;
-    this.quSites = new WeightedQuickUnionUF(n * n + 2); // add two dummies(virtual top and bottom)
     this.sites = new boolean[n * n];
+    this.quSites = new WeightedQuickUnionUF(n * n + 2); // add two dummies(virtual top and bottom)
+
+    this.vTop = n * n;
+    this.vBottom = n * n + 1;
 
     for (int i = 0; i < n * n; i++) {
       this.sites[i] = false; // False:blocked,True:open
     }
 
-    // connect virtual top site
     for (int i = 0; i < n; i++) {
-      this.quSites.union(n * n, i);
-    }
-    // connect virtual bottom site
-    for (int i = (n == 1) ? 0 : (0 + n * n - n - 1); i < n; i++) {
-      this.quSites.union(i, n * n + 1);
+      // connect virtual top site
+      this.quSites.union(i, vTop);
+      // connect virtual bottom site
+      this.quSites.union(n== 1 ? 0 : xy2flat(this.n, i), vBottom);
     }
   }
 
@@ -50,61 +55,39 @@ public class Percolation {
     if (isValid(row, col) && !isOpen(row, col)) {
       // System.out.printf("Open site[%d, %d]\n", row, col);
       this.sites[xy2flat(row, col)] = true;
-
-      if (row == 0) {
-        // System.out.printf("Connect Dummy Top\n");
-        this.quSites.union(xy2flat(row, col), n * n);
-      } else if (row == n) {
-        // System.out.printf("Connect Dummy Bottom\n");
-        this.quSites.union(xy2flat(row, col), n * n + 1);
-      }
-
       openCross(row, col);
     }
   }
 
   private void openCross(int row, int col) {
-    if (isValid(row - 1, col) && isOpen(row - 1, col)) {
+    if (1 < row && isOpen(row - 1, col)) {
       // System.out.printf("Connect up site\n");
       this.quSites.union(xy2flat(row, col), xy2flat(row - 1, col));
     }
 
-    if (isValid(row + 1, col) && isOpen(row + 1, col)) {
+    if (row < this.n && isOpen(row + 1, col)) {
       // System.out.printf("Connect down site\n");
       this.quSites.union(xy2flat(row, col), xy2flat(row + 1, col));
     }
 
-    if (isValid(row, col - 1) && isOpen(row, col - 1)) {
+    if (1 < col && isOpen(row, col - 1)) {
       // System.out.printf("Connect left site\n");
       this.quSites.union(xy2flat(row, col), xy2flat(row, col - 1));
     }
 
-    if (isValid(row, col + 1) && isOpen(row, col + 1)) {
+    if (col < this.n && isOpen(row, col + 1)) {
       // System.out.printf("Connect right site\n");
       this.quSites.union(xy2flat(row, col), xy2flat(row, col + 1));
     }
   }
 
   public boolean isOpen(int row, int col) { // is site (row, col) open?
-    try {
-      validateIndex(row, col);
-      return this.sites[xy2flat(row, col)];
-    } catch (IndexOutOfBoundsException e) {
-      // System.out.println("Error " + e.getMessage());
-      // e.printStackTrace();
-      return this.sites[xy2flat(row, col)];
-    }
+    return this.sites[xy2flat(row, col)];
   }
 
   public boolean isFull(int row, int col) { // is site (row, col) full?
-    try {
-      validateIndex(row, col);
-      return this.quSites.connected(0, xy2flat(row, col));
-    } catch (IndexOutOfBoundsException e) {
-      // System.out.println("Error " + e.getMessage());
-      // e.printStackTrace();
-      return false;
-    }
+    validateIndex(row, col);
+    return isOpen(row, col) && this.quSites.connected(vTop, xy2flat(row, col));
   }
 
   public int numberOfOpenSites() { // number of open sites
@@ -121,12 +104,14 @@ public class Percolation {
   }
 
   public boolean percolates() { // does the system percolate?
-    return this.quSites.connected(n * n, n * n + 1);
+      return this.quSites.connected(vTop, vBottom);
   }
 
   public static void main(String[] args) { // test client (optional)
     test1();
     test2();
+    test3();
+    test4();
   }
 
   private static void test1() {
@@ -155,19 +140,54 @@ public class Percolation {
     System.out.printf("Test2\n");
     final Percolation p = new Percolation(3);
 
-    System.out.println("p.isOpen(1, 2) = " + p.isOpen(1, 2));
+    System.out.println("p.isOpen(1, 2) ? " + p.isOpen(1, 2));
     p.open(1, 2);
     System.out.println("p.isOpen(1, 2) = " + p.isOpen(1, 2));
 
-    System.out.println("p.isOpen(2,2) = " + p.isOpen(2, 2));
+    System.out.println("p.isOpen(2,2) ? " + p.isOpen(2, 2));
     p.open(2, 2);
     System.out.println("p.isOpen(2,2) = " + p.isOpen(2, 2));
     System.out.println("p.isFull(2, 2) = " + p.isFull(2, 2));
 
-    System.out.println("p.isOpen(3, 2) = " + p.isOpen(3, 2));
+    System.out.println("p.isOpen(3, 2) ? " + p.isOpen(3, 2));
     p.open(3, 2);
     System.out.println("p.isOpen(3, 2) = " + p.isOpen(3, 2));
     p.isFull(3, 2);
+
+    System.out.println("p.percolates() = " + p.percolates());
+    System.out.println("p.numberOfOpenSites() = " + p.numberOfOpenSites());
+  }
+
+  private static void test3() {
+    System.out.printf("Test3\n");
+    final Percolation p = new Percolation(2);
+
+    System.out.println("p.isOpen(1, 2) ? " + p.isOpen(1, 2));
+    p.open(1, 2);
+    System.out.println("p.isOpen(1, 2) = " + p.isOpen(1, 2));
+
+    System.out.println("p.isOpen(2,2) ? " + p.isOpen(2, 2));
+    p.open(2, 2);
+    System.out.println("p.isOpen(2,2) = " + p.isOpen(2, 2));
+    System.out.println("p.isFull(2, 2) = " + p.isFull(2, 2));
+
+    System.out.println("p.isOpen(2, 1) ? " + p.isOpen(2, 1));
+    p.open(2, 1);
+    System.out.println("p.isFull(2, 1) = " + p.isFull(2, 1));
+
+    System.out.println("p.percolates() = " + p.percolates());
+    System.out.println("p.numberOfOpenSites() = " + p.numberOfOpenSites());
+  }
+
+  private static void test4() {
+    System.out.printf("Test4\n");
+    final Percolation p = new Percolation(1);
+
+    System.out.println("p.isOpen(1, 1) ? " + p.isOpen(1, 1));
+    p.open(1, 1);
+    System.out.println("p.isOpen(1, 1) = " + p.isOpen(1, 1));
+
+    System.out.println("p.isFull(1, 1) ? " + p.isFull(1, 1));
 
     System.out.println("p.percolates() = " + p.percolates());
     System.out.println("p.numberOfOpenSites() = " + p.numberOfOpenSites());
