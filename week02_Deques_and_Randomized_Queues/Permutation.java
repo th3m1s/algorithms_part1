f4aefc13cd38cac9c76c511b6e294f8d3e62fcc2
Yuto Tagashira <gh0st1nth3sh311.dev@gmail.com>
2018-05-23 00:46:33 +0900

    update: add declaration of required API

---
+++ week02_Deques_and_Randomized_Queues/Permutation.java
@@ -0,0 +1,6 @@
+public class Permutation {
+   public static void main(String[] args)
+}
+
+
+// Your submission may not call library functions except those in StdIn, StdOut, StdRandom, java.lang, java.util.Iterator, and java.util.NoSuchElementException. In particular, do not use either java.util.LinkedList or java.util.ArrayList.
