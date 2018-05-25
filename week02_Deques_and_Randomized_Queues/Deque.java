000bec82ae3a4700687ff42e3522b1fea4290ad2
Yuto Tagashira <gh0st1nth3sh311.dev@gmail.com>
2018-05-25 22:09:45 +0900

    update: update deque.java

--- week02_Deques_and_Randomized_Queues/Deque.java
+++ week02_Deques_and_Randomized_Queues/Deque.java
@@ -1,14 +1,60 @@
 public class Deque<Item> implements Iterable<Item> {
-   public Deque(){                           // construct an empty deque
 
-   }
+  private Node head, tail;
+  private int n;
 
-   public boolean isEmpty()                 // is the deque empty?
-   public int size()                        // return the number of items on the deque
-   public void addFirst(Item item)          // add the item to the front
-   public void addLast(Item item)           // add the item to the end
-   public Item removeFirst()                // remove and return the item from the front
-   public Item removeLast()                 // remove and return the item from the end
-   public Iterator<Item> iterator()         // return an iterator over items in order from front to end
-   public static void main(String[] args)   // unit testing (optional)
+  private class Node {
+    Item item;
+    Node pre;
+    Node next;
+  }
+
+  public Deque() {// construct an empty deque
+    head = null;
+    tail = null;
+    n    = 0;
+  }
+
+  public boolean isEmpty() {// is the deque empty?
+    return n == 0;
+  }
+
+  public int size() {// return the number of items on the deque
+    return n;
+  }
+
+  public void addFirst(Item item) {// add the item to the front
+    if (item == null) { throw new java.lang.NullPointerException(); }
+    Node oldhead = head;
+    head.item    = item;
+    head.pre     = null;
+    head.next    = oldhead;
+    n++;
+  }
+
+  public void addLast(Item item) {// add the item to the end
+    if (item == null) { throw new java.lang.NullPointerException(); }
+    Node oldtail = tail;
+    tail.item    = item;
+    tail.pre     = oldtail;
+    oldtail.next = tail;
+    tail.next    = null;
+    n++;
+  }
+
+  public Item removeFirst() {// remove and return the item from the front
+    if (n == 0) { throw java.util.NoSuchElementException(); }
+    head.item = head.next;
+    head.next = ;
+    n--;
+  }
+
+  public Item removeLast() {// remove and return the item from the end
+  }
+
+  public Iterator<Item> iterator() {// return an iterator over items in order from front to end
+  }
+
+  public static void main(String[] args) {// unit testing (optional)
+  }
 }
